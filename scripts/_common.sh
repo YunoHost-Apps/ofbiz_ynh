792be98ab0fdff6fea2b0fa815d97480b8df5f6b
Félix Piédallu <felix@piedallu.me>
2024-03-22 22:21:58 +0100

    Fix nginx

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,8 +9,9 @@
 #=================================================
 
 _ci_fix_nginx() {
+    # Ofbiz returns a 404 on its root, so let's redirect to another path also served
     if [ "${PACKAGE_CHECK_EXEC:-0}" -eq 1 ]; then
-        echo 'rewrite ^/$ /catalog/;' >> "/etc/nginx/conf.d/${domain}.d/$app.conf"
+        echo 'rewrite ^/$ /catalog/;' >> "../conf/nginx.conf"
     fi
 }
 
