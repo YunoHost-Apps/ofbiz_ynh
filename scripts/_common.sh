cbcf23c8e58ed31366511317723f37ecc37753e3
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-11-03 19:36:27 +0100

    convert to helpers2.1

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,38 +1,26 @@
 #!/bin/bash
 
 #=================================================
-# COMMON VARIABLES
-#=================================================
-
-#=================================================
-# PERSONAL HELPERS
+# COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
 
 _ci_fix_nginx() {
     # Ofbiz returns a 404 on its root, so let's redirect to another path also served
-    if [ "${PACKAGE_CHECK_EXEC:-0}" -eq 1 ]; then
+    if ynh_in_ci_tests; then
         echo 'rewrite ^/$ /catalog/;' >> "../conf/nginx.conf"
     fi
 }
 
 _fixup_config() {
-    ynh_add_config --template="url.properties" --destination="$install_dir/build/framework/webapp/config/url.properties"
-    ynh_delete_file_checksum --file="$install_dir/build/framework/webapp/config/url.properties"
+    ynh_config_add --template="url.properties" --destination="$install_dir/build/framework/webapp/config/url.properties"
+    ynh_delete_file_checksum "$install_dir/build/framework/webapp/config/url.properties"
 
-    ynh_add_config --template="security.properties" --destination="$install_dir/build/framework/security/config/security.properties"
-    ynh_delete_file_checksum --file="$install_dir/build/framework/security/config/security.properties"
+    ynh_config_add --template="security.properties" --destination="$install_dir/build/framework/security/config/security.properties"
+    ynh_delete_file_checksum "$install_dir/build/framework/security/config/security.properties"
 
-    ynh_add_config --template="catalina-component.xml" --destination="$install_dir/build/framework/catalina/ofbiz-component.xml"
-    ynh_delete_file_checksum --file="$install_dir/build/framework/catalina/ofbiz-component.xml"
+    ynh_config_add --template="catalina-component.xml" --destination="$install_dir/build/framework/catalina/ofbiz-component.xml"
+    ynh_delete_file_checksum "$install_dir/build/framework/catalina/ofbiz-component.xml"
 
-    ynh_add_config --template="entity-config.xml" --destination="$install_dir/build/framework/entity/config/entityengine.xml"
-    ynh_delete_file_checksum --file="$install_dir/build/framework/entity/config/entityengine.xml"
+    ynh_config_add --template="entity-config.xml" --destination="$install_dir/build/framework/entity/config/entityengine.xml"
+    ynh_delete_file_checksum "$install_dir/build/framework/entity/config/entityengine.xml"
 }
-
-#=================================================
-# EXPERIMENTAL HELPERS
-#=================================================
-
-#=================================================
-# FUTURE OFFICIAL HELPERS
-#=================================================
